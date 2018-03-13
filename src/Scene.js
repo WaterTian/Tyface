08426457920d25ef5686fabdb4365bd7a7b7cfce
WaterTian <changewater@qq.com>
2018-03-13 12:32:35 +0800

    add black

--- src/Scene.js
+++ src/Scene.js
@@ -14,7 +14,8 @@ var facePlaneArr = [];
 var imageW, imageH;
 
 
-var maskImage, maskImageCtx, maskTexture;
+var maskImageCtx, maskTexture;
+var blackCtx, blackTexture;
 
 
 export default class Scene {
@@ -49,12 +50,20 @@ export default class Scene {
 		imageH = That.face.faceImage.height;
 
 
-		//mask
-		maskImage = document.createElement('canvas');
-		maskImageCtx = maskImage.getContext("2d");
-		maskTexture = PIXI.Texture.fromCanvas(maskImage);
-		maskImage.width = imageW;
-		maskImage.height = imageH;
+		//maskFace
+		var mask = document.createElement('canvas');
+		maskImageCtx = mask.getContext("2d");
+		maskTexture = PIXI.Texture.fromCanvas(mask);
+		mask.width = imageW;
+		mask.height = imageH;
+
+
+		//maskBlack
+		var black = document.createElement('canvas');
+		blackCtx = black.getContext("2d");
+		blackTexture = PIXI.Texture.fromCanvas(black);
+		black.width = imageW;
+		black.height = imageH;
 
 
 		var faceNum = 5;
@@ -68,12 +77,21 @@ export default class Scene {
 				facePlaneArr.push(facePlane);
 				APP.stage.addChild(facePlane);
 			} else {
+
+				var faceBlack = new PIXI.Sprite(blackTexture);
+				faceBlack.scale.set(c, c*1.2);
+				faceBlack.x = imageW * (1 - c) / 2;
+				faceBlack.y = imageH * (1 - c) / 2;
+				facePlaneArr.push(faceBlack);
+				APP.stage.addChild(faceBlack);
+
 				var faceMask = new PIXI.Sprite(maskTexture);
 				faceMask.scale.set(c, c);
 				faceMask.x = imageW * (1 - c) / 2;
 				faceMask.y = imageH * (1 - c) / 2;
 				facePlaneArr.push(faceMask);
 				APP.stage.addChild(faceMask);
+
 			}
 		}
 
@@ -94,7 +112,11 @@ export default class Scene {
 		if (faces.length > 0) {
 			var face = faces[0];
 
+			var centerX = face.vertices[30 * 2];
+			var centerY = face.vertices[30 * 2 + 1];
+			var _h = face.vertices[8 * 2 + 1] - centerY;
 
+            //////////
 			maskImageCtx.clearRect(0, 0, imageW, imageH);
 			maskImageCtx.fillStyle = "red";
 			maskImageCtx.beginPath()
@@ -103,24 +125,36 @@ export default class Scene {
 				maskImageCtx.moveTo(face.vertices[0], face.vertices[1]);
 				for (var k = 2; k < 17 * 2; k += 2) {
 					maskImageCtx.lineTo(face.vertices[k], face.vertices[k + 1]);
+					// maskImageCtx.quadraticCurveTo(face.vertices[k], face.vertices[k + 1], face.vertices[k + 2], face.vertices[k + 3]);
 				}
 				for (k = 26 * 2; k >= 17 * 2; k -= 2) {
 					maskImageCtx.lineTo(face.vertices[k], face.vertices[k + 1]);
 				}
 			}
 			maskImageCtx.fill();
+			maskImageCtx.closePath();
 			maskImageCtx.globalCompositeOperation = "source-out";
 			maskImageCtx.drawImage(That.face.faceImage, 0, 0);
-			// maskImageCtx.fillStyle = "rgba(0,0,0,0.3)";
-			// maskImageCtx.fillRect(0, 0, imageW, imageH);
 			maskTexture.update();
 
+
+			/////////
+			var grd = blackCtx.createRadialGradient(centerX, centerY, _h*0.6, centerX, centerY, _h*1.2);
+			grd.addColorStop(0, "rgba(0,0,0,0)");
+			grd.addColorStop(1, "rgba(0,0,0,1)");
+			blackCtx.fillStyle = grd;
+			blackCtx.clearRect(0, 0, imageW, imageH);
+			blackCtx.fillRect(0, 0, imageW, imageH);
+			blackTexture.update();
+
+
 			for (var i = 0; i < facePlaneArr.length; i++) {
-				var c = facePlaneArr[i].scale.x;
-				facePlaneArr[i].x += (face.vertices[30 * 2] * (1 - c) - facePlaneArr[i].x) * 0.15 * (i + 1);
-				facePlaneArr[i].y += (face.vertices[30 * 2 + 1] * (1 - c) - facePlaneArr[i].y) * 0.15 * (i + 1);
+				var cx = facePlaneArr[i].scale.x;
+				var cy = facePlaneArr[i].scale.y;
+				facePlaneArr[i].x += (centerX * (1 - cx) - facePlaneArr[i].x) * 0.15 * (i + 1);
+				facePlaneArr[i].y += (centerY * (1 - cy) - facePlaneArr[i].y) * 0.15 * (i + 1);
 			}
-			
+
 		}
 
 
